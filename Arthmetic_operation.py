94d9f1cf76757585c4ca4259c78206e1f57b0b2a
apankajyadav <50950259+apankajyadav@users.noreply.github.com>
2026-08-06 14:04:22 +0530

    Create Arthmetic_operation.py
    In this I am learn Working with Arithmetic operation like addition ,subtraction,multiplication etc..

---
+++ Arthmetic_operation.py
@@ -0,0 +1,10 @@
+# Operator in python
+a = 100
+b = 5
+print(a + b) # Addition 
+print(a - b) # Subtraction 
+print(a * b) # Multiplication 
+print(a / b) # Division 
+print(a % b) # Modulus (remainder)
+print(a ** b) # Power
+print(a // b) #Floor Division 
