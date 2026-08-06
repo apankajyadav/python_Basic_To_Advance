cf6d47e5db9531ddc131d4fe953ae71cfec3c635
apankajyadav <50950259+apankajyadav@users.noreply.github.com>
2026-08-06 13:55:10 +0530

    Variable_Creation.py

---
+++ Variable.py
@@ -0,0 +1,17 @@
+# String variable
+name = "Pankaj"
+
+# Integer variable
+age = 30
+
+# Float variable
+salary = 55000.75
+
+# Boolean variable
+is_employee = True
+
+# Print variables
+print(name)
+print(age)
+print(salary)
+print(is_employee)
