254fe598225c9241a765d50aa8e7ff80922ae205
apankajyadav <50950259+apankajyadav@users.noreply.github.com>
2026-08-12 20:22:14 +0530

    Create List_Exercises1.py
    Ex1_to_Ex50

---
+++ Exercise_List/List_Exercises1.py
@@ -0,0 +1,180 @@
+
+# Ex1. Create an empty list.
+Empty_list=[]
+
+# Ex2. Create a list containing 5 integers.
+int_list=[8,2,3,45,9]
+
+# Ex3. Create a list containing 5 strings.
+string_list=['name','surname','fruit','vegitables','game']
+
+# Ex4. Find the length of a list.
+print(len(string_list))
+
+# Ex5. Access the first element of a list.
+print(string_list[0])
+
+# Ex6. Access the last element of a list.
+print(string_list[-1])
+
+# Ex7. Access an element using a positive index.
+print(string_list[1])
+
+# Ex8. Access an element using a negative index.
+print(string_list[-2])
+
+# Ex9. Get the first 3 elements using slicing.
+word='Welcome'
+print(word[0:3])
+
+# Ex10. Get the last 3 elements using slicing.
+print(word[-3:])
+
+# Ex11. Get all elements except the first element.
+print(word[1:])
+
+# Ex12. Get all elements except the last element.
+print(word[:6])
+
+# Ex13. Reverse a list using slicing.
+rev_int_lst=int_list[::-1]
+print(rev_int_lst)
+
+# Ex14. Add an element to the end using append().
+string_list.append('welcomeworld')
+print(string_list)
+
+# Ex15. Add multiple elements using extend().
+string_list.extend(['park','lion','www'])
+print(string_list)
+
+# Ex16. Insert an element at a specific index.
+string_list.insert(2,"myname")
+print(string_list)
+
+# Ex17. Remove an element using remove().
+string_list.remove('game')
+print(string_list)
+
+# Ex18. Remove the last element using pop().
+print(string_list.pop())
+print(string_list)
+
+# Ex19. Remove an element using its index with pop().
+print(string_list.pop(4))
+
+# Ex20. Delete an element using del.
+del string_list[2]
+print(string_list)
+
+# Ex21. Delete a slice of elements using del.
+print(string_list)
+del string_list[1:3]
+print(string_list)
+
+# Ex22. Clear a list.
+string_list.clear()
+print(string_list)
+
+# Ex23. Find the index of an element using index().
+print(string_list.index('game'))
+
+# Ex24. Count occurrences of an element using count().
+occ_of_ele=string_list.count('name')
+print(occ_of_ele)
+print(string_list)
+string_list.append('name')
+print(string_list)
+occ_of_ele=string_list.count('name')
+print(occ_of_ele)
+
+# Ex25. Check whether an element exists in a list.
+print('game' in string_list)
+
+# Ex26. Check whether an element does not exist in a list.
+print('myself' not in string_list)
+print('name' not in string_list)
+
+# Ex27. Copy a list using copy().
+newstr_lst=string_list.copy()
+
+# Ex28. Copy a list using slicing.
+new_str_lst_sliceer=string_list[:]
+print(new_str_lst_sliceer)
+
+# Ex29. Copy a list using list().
+new_list_using_list=list(string_list)
+print(new_list_using_list)
+
+# Ex30. Concatenate two lists using +.
+list2=['lion','cat','dog']
+con_list=string_list + list2
+print(con_list)
+
+# Ex31. Repeat a list using *.
+rep_list=int_list*2
+print(rep_list)
+
+# Ex32. Sort a list using sort().
+int_list.sort()
+print(int_list)
+
+# Ex33. Sort a list in descending order.
+desc_list=sorted(int_list,reverse=True)
+print(desc_list)
+
+# Ex34. Reverse a list using reverse().
+string_list.reverse()
+print(string_list)
+
+# Ex35. Find the minimum element using min().
+min_val=min(int_list)
+print(min_val)
+
+# Ex36. Find the maximum element using max().
+maxx=max(int_list)
+print(maxx)
+
+# Ex37. Find the sum of numeric list elements using sum().
+print(int_list)
+sum_val=sum(int_list)
+print(sum_val)
+
+# Ex38. Find the length of a nested list.
+new_lst=[[8 ,9] ,[2, 85, 77], [1, 1, 1, 1, 1, 1, 1]]
+for i in new_lst:
+ print(len(i))
+
+# Ex39. Access an element from a nested list.
+new_lst=[[8 ,9] ,[2, 85, 77], [1, 1, 1, 5, 1, 1, 1]]
+print(new_lst[2][3])
+
+# Ex40. Access an element from a 2-dimensional list.
+list_nested=[
+ [1,2,3],
+ [4,5,6],
+ [7,8,9]
+]
+
+# Ex41. Change an element at a specific index.
+list_nested[0][1]=0
+print(list_nested)
+
+# Ex42. Change multiple elements using slicing.
+list_nested[1][0:3]=[40,50,60]
+print(list_nested)
+
+# Ex43. Replace one element with multiple elements using slicing.
+list_nested[1][1:2]=[100,200]
+print(list_nested)
+
+# Ex44. Replace multiple elements with fewer elements using slicing.
+list_nested[0][1:3]=[500]
+print(list_nested)
+
+# Ex45. Add an element at index 0.
+list_nested[0].insert(0,900)
+print(list_nested)
+
+int_list.insert(0,2000)
+print(int_list)
