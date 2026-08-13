1b7284c800f5b3abeb876e5bcf13c1961256081e
apankajyadav <50950259+apankajyadav@users.noreply.github.com>
2026-08-13 16:58:52 +0530

    Add files via upload
    While_Exercise

---
+++ Exercise_List/While_loop_ex/reverse_oder.py
@@ -0,0 +1,5 @@
+s_num=10
+l_num=1
+while(s_num>=l_num):
+    print(s_num)
+    s_num-=1
---
+++ Exercise_List/While_loop_ex/table.py
@@ -0,0 +1,6 @@
+T=int(input("Enter Number You want to print Table: "))
+n=1
+while n <=10:
+    
+    print(f"{T} X {n} ={T*n}")
+    n=n+1
