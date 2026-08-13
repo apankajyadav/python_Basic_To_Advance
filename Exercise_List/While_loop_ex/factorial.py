27d1e93aef3a0199148246addd8c611fe915c94d
apankajyadav <50950259+apankajyadav@users.noreply.github.com>
2026-08-13 20:41:30 +0530

    Add files via upload
    While_loop_2nd part_execise

---
+++ Exercise_List/While_loop_ex/factorial.py
@@ -0,0 +1,7 @@
+number=int(input("Enter Number you want to find factorial : "))
+n=1
+fact=1
+while(n<=number):
+    fact=fact*n
+    n+=1
+print(fact)
---
+++ Exercise_List/While_loop_ex/sum_of_even.py
@@ -0,0 +1,8 @@
+number=int(input("Enter Number : "))
+n=1
+sum=0
+while(n<=number):
+    if n%2==0:
+        sum=sum+n
+    n+=1
+print(f"sum of Even number is {sum}")
