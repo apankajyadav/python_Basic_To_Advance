a2717815cfb64322ead09df6b56ee1c73cf38a3e
Pankaj Yadav <apankajyadav7507@gmail.com>
2026-08-16 10:38:48 +0530

    Find Square of Number

---
+++ Exercise_List/While_loop_ex/cube.py
@@ -0,0 +1,8 @@
+#find Cube of number
+Num=int(input("Enter Starting number : "))
+end=int(input("Etere end number : "))
+result=0
+while (Num<=end):
+    result=Num**3
+    print(f"{Num}\u00B3 = {result}")
+    Num+=1
