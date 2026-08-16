614708fc4f93abaeafcd9940e11feeea706fb93b
Pankaj Yadav <apankajyadav7507@gmail.com>
2026-08-16 10:29:15 +0530

    Find Square of Number

---
+++ Exercise_List/While_loop_ex/square.py
@@ -0,0 +1,8 @@
+Num=int(input("Enter Starting number : "))
+end=int(input("Etere end number : "))
+result=0
+while (Num<=end):
+    result=Num*Num
+    print(f"{Num} x {Num} = {result}")
+    Num+=1
+
