1b5bdc6cfd285f4b229d8259e91fcdcc78ce5b23
Pankaj Yadav <apankajyadav7507@gmail.com>
2026-08-17 19:02:28 +0530

    Take Input until user Enter Negtive number

---
+++ Exercise_List/for_loop_Ex/negative_exit.py
@@ -0,0 +1,12 @@
+count=0
+for _ in iter(int,1):
+    Num=int(input("Enter Number :"))
+    if Num<0:
+        break
+    else:
+        count+=1
+print(f"Total positive input enter by Use {count}")
+    
+   
+
+    
